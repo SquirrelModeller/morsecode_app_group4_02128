df87f207434a7d115784d12266b9f47ec97f8092
karl1424 <karlagerbo@gmail.com>
2024-06-20 13:40:01 +0200

    fix: fixed navigation bar not showing on training
    fix: merge error

--- morsetorch/lib/screens/morse_training_selector.dart
+++ morsetorch/lib/screens/morse_training_selector.dart
@@ -10,49 +10,56 @@ class MorseTrainingSelectorPage extends StatefulWidget {
 }
 
 class MorseTrainingSelectorPageState extends State<MorseTrainingSelectorPage> {
+  bool advanced = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            Padding(
-              padding:  EdgeInsets.only(left: MediaQuery.of(context).size.width / 9),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                height: MediaQuery.of(context).size.width / 3,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Beginner',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 21),
+    return advanced
+        ? MorseTrainingPage()
+        : Scaffold(
+            body: Center(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width / 9),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      height: MediaQuery.of(context).size.width / 3,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Beginner',
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 21),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        right: MediaQuery.of(context).size.width / 9),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      height: MediaQuery.of(context).size.width / 3,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            advanced = true;
+                          });
+                        },
+                        child: Text(
+                          'Advanced',
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 21),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Spacer(),
-            Padding(
-              padding: EdgeInsets.only(right: MediaQuery.of(context).size.width / 9),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                height: MediaQuery.of(context).size.width / 3,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MorseTrainingPage()),
-                    );
-                  },
-                  child: Text('Advanced', style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 21),),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
