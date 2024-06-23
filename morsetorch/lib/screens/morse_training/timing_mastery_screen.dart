0739e9a3941fa803eee5474f1553f5f266122584
karl1424 <karlagerbo@gmail.com>
2024-06-23 21:44:06 +0200

    fix: change class name

--- morsetorch/lib/screens/morse_training/timing_mastery_screen.dart
+++ morsetorch/lib/screens/morse_training/timing_mastery_screen.dart
@@ -12,10 +12,10 @@ class TimingMasteryScreen extends StatefulWidget {
       {super.key, required this.setScreen, required this.isDarkMode});
 
   @override
-  _MorsePracticeTimingState createState() => _MorsePracticeTimingState();
+  _TimingMasteryScreenState createState() => _TimingMasteryScreenState();
 }
 
-class _MorsePracticeTimingState extends State<TimingMasteryScreen> {
+class _TimingMasteryScreenState extends State<TimingMasteryScreen> {
   late TimingMasteryService timingMasteryService;
   Color buttonColor = Colors.grey;
   Timer? colorUpdateTimer;
